5aa3e93ad8cc27e9c59f5fd8a32006ccbbda14a7
Verbindolai <nikolaiwieczorek@web.de>
2020-12-17 00:22:49 +0100

    Improved Project Structure and added JSDoc

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,25 @@
 import {SketchServer} from "./server"
 import {Client} from "./Client"
 
-let Server : SketchServer = new SketchServer(6969);
-Server.start();
+/**
+ * Entrypoint
+ * @author Christopher Peters, Mathusan Kannathasan, Nikolai Wieczorek
+ * @version 1.0
+ */
+class Main {
 
-let client : Client = new Client();
-client.send()
-client.handle()
-client.send()
+    /**
+     * Starts the Server
+     */
+    public static main () : void{
+        let Server : SketchServer = new SketchServer(6969);
+        Server.start();
+
+        let client : Client = new Client(Server.port);
+        client.send()
+        client.handle()
+        client.send()
+    }
+}
+Main.main();
+
--- src/server.ts
+++ src/server.ts
@@ -2,41 +2,88 @@ import express, {Express, Request, Response} from "express";
 import {Server as SocketServer, Socket} from "socket.io";
 import {Server as HTTPServer} from "http"
 
+/**
+ * Represents the Sketch-Server
+ * @author Christopher Peters, Mathusan Kannathasan, Nikolai Wieczorek
+ * @version 1.0
+ */
 export class SketchServer {
 
-    app : Express;
-    port : number;
-    io: SocketServer;
+    private app : Express;
+    private readonly _port : number;
+    private io: SocketServer;
 
     constructor(port : number) {
-        this.port = port;
+        this._port = port;
         this.app = express();
-        this.io = new SocketServer(this.startServer(this.port));
+        this.io = new SocketServer(this.startServer(this._port));
     }
 
-    startServer(port : number) : HTTPServer{
+    /**
+     * Getter for the Port
+     */
+    public get port(){
+        return this._port;
+    }
+
+    /**
+     * Returns a HTTP-Server Object
+     * @param port : port to listen on
+     * @private
+     */
+
+    private startServer(port : number) : HTTPServer{
         console.log(`Listening on Port ${port} ...`)
         return this.app.listen(port);
     }
 
-    init (): void{
-
+    /**
+     * Initialises something
+     * @private
+     */
+    private init (): void{
 
     }
 
-    start (): void {
-        this.app.get('/', function (req : Request, res : Response) {
-            res.sendFile("C:/Mega/Dev/sketcholai/src/index.html");
-        });
+    /**
+     * Starts the Request- and Websocket-Handling
+     */
+    public start (): void {
+        this.init();
+        this.getRequestHandler();
+        this.websocketHandler();
+    }
 
+    /**
+     * Handles GET-Request to the HTTP-Server
+     * @private
+     */
+    private getRequestHandler() : void{
+        this.app.get('/', function (req : Request, res : Response) {
+            res.sendFile("C:/Mega/Dev/sketcholai/src/index.html", (err) => {
+                if (err){
+                    console.error("There was an error sending the Response-File.")
+                } else {
+                    console.log("Response-File transmitted.")
+                }
+            });
+        });
+    }
+
+    /**
+     * Handles Websocket-Events
+     * @private
+     */
+    private websocketHandler() : void {
         this.io.on('connection', function (socket : Socket) {
             console.log("New Connection!")
-            socket.emit('news', "fick dich");
+            socket.emit('news', "Hey, from the Server!");
             socket.on('message',  (data) => {
                 console.log(`Server received Message:\n${data}`);
             });
         });
-
     }
+
+
 }
 
