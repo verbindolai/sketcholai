d2de3b60b4d9c154895a82578b40b531f9501a4f
Verbindolai <nikolaiwieczorek@web.de>
2020-12-16 22:49:16 +0100

    SocketIO communication initialised. Client and Server can send and receive messages.

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,23 @@
-import {Express, Request, Response} from "express";
+import express, {Express, Request, Response} from "express";
 import {Server as SocketServer, Socket} from "socket.io";
+import {Server as HTTPServer} from "http"
 
-class SketchServer {
+export class SketchServer {
 
     app : Express;
+    port : number;
     io: SocketServer;
 
-    constructor() {
-        this.app = require('express').createServer();
-        this.io = require('socket.io')(this.app);
-        this.app.listen(6969);
+    constructor(port : number) {
+        this.port = port;
+        this.app = express();
+        this.io = new SocketServer(this.startServer(this.port));
     }
 
+    startServer(port : number) : HTTPServer{
+        console.log(`Listening on Port ${port} ...`)
+        return this.app.listen(port);
+    }
 
     init (): void{
 
@@ -20,17 +26,17 @@ class SketchServer {
 
     start (): void {
         this.app.get('/', function (req : Request, res : Response) {
-            res.send(__dirname + '/index.html');
+            res.sendFile("C:/Mega/Dev/sketcholai/src/index.html");
         });
 
         this.io.on('connection', function (socket : Socket) {
+            console.log("New Connection!")
             socket.emit('news', "fick dich");
-            socket.on('message', function (data) {
-                console.log(data);
+            socket.on('message',  (data) => {
+                console.log(`Server received Message:\n${data}`);
             });
         });
 
     }
 }
-let gay : SketchServer = new SketchServer();
-gay.start();
+
