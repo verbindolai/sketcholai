7df0739676912a2554a3bca44eed2c8ef19057d4
Verbindolai <nikolaiwieczorek@web.de>
2020-12-17 23:33:28 +0100

    Added second tsconfig.json for client

---
+++ src/server/main.ts
@@ -0,0 +1,18 @@
+import {SketchServer} from "./server"
+/**
+ * Entrypoint
+ * @author Christopher Peters, Mathusan Kannathasan, Nikolai Wieczorek
+ * @version 1.0
+ */
+class Main {
+
+    /**
+     * Starts the Server
+     */
+    public static main () : void{
+        let Server : SketchServer = new SketchServer(6969);
+        Server.start();
+    }
+}
+Main.main();
+
